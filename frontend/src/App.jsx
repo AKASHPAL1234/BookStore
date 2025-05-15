e9e529dcb571e692c5fd66b6b296f16cbb0541b6
akash pal <palankit86762@gmail.com>
2025-05-15 15:14:29 +0530

    Updated the project for redeploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Home from "./Home/Home";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Coures from "./component/coureses/Coures";
 import Signup from "./component/Signup";
 import Contact from "./component/Contect";
 import  { Toaster } from 'react-hot-toast';
 import { useAuth } from "./context/AuthProvider";
+import Course from "./component/Course";
 
 
 function App() {
@@ -16,7 +16,9 @@ function App() {
       <div className="dark:bg-slate-900 dark:text-white ">
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/course" element={authUser?<Coures />:<Navigate to="/signup"></Navigate>}></Route>
+          <Route path="/course" element={<Course />}></Route>
+
+          {/* <Route path="/course" element={authUser?<Coures />:<Navigate to="/signup"></Navigate>}></Route> */}
           <Route path="/signup" element={<Signup />}></Route>
           <Route path="/contect" element={<Contact />}></Route>
 
